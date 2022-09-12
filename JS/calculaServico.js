b346234fa199d44cd0ba7e18c79c9a029c50dc3e
Luís Felipe Galvão <88863600+Sasuzin@users.noreply.github.com>
2022-09-12 19:38:21 -0300

    Update calculaServico.js

--- JS/calculaServico.js
+++ JS/calculaServico.js
@@ -1,21 +1,32 @@
-let tabela =  [['$$$', '$$','$'],
-              ['$$$$' ,'$$' , '$$'],
-              ['--','--','$$$']];
+/* cobrado do cliente.
+O valor do serviço pode variar conforme o 
+prazo.
+Os valores e prazos devem ser armazenados 
+em uma matriz.
+O cálculo deve ficar obrigatoriamente em 
+uma funçãoe
+*/
 
-let servico = prompt ("entre com o servico \n0 - troca de óleo \n1 - balanceamento \n2 - cambagem");                
-let prazo = prompt ("entre com o prazo: \n0 - um dia \n1 - dois dias \n2 - tres dias");
+let tabela = [  ['$$$','$$','$'],
+                ['$$$$','$$$','$$'],
+                ['Não faz','Não faz','$$$']];
 
-if ( Validaentrada(servico) && Validaentrada(prazo)) {
+let servico = prompt("Entre com o serviço: \n0 - Troca de óleo \n1 - Balanceamento\n 2 - Cambagem ");
+let prazo = prompt("Entre com o prazo: \n0 - Um dia \n1 - Dois dias\n 2 - Três dias ");
 
-    alert("O serviços ficará em: " + tabela[servico][prazo]);
+if( validaEntrada(servico) && validaEntrada(prazo) ){
 
-}else {
+    alert("O serviço ficará em: " + tabela[servico][prazo]);
+
+}else{
 
     alert("ERRO: Verifique os valores inseridos e tente novamente!");
 }
-function Validaentrada(dadoUsuario){
+
+function validaEntrada(dadoUsuario){
 
     return dadoUsuario >= 0 && dadoUsuario <= 2 ? true : false;
 }
+
 
 
