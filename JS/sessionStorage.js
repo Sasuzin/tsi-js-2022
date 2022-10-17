ef9d50e8d9106243d4178ec6ead70e5acad0055d
Your Name <you@example.com>
2022-10-17 17:14:11 -0300

    Finalização de dom e Storages

---
+++ JS/sessionStorage.js
@@ -0,0 +1,28 @@
+//Como trabalhar com SESSION STORAGE
+
+let btn = document.getElementById('botao');
+
+btn.addEventListener('click', mostrar);
+
+function mostrar(evento){
+    evento.preventDefault();
+
+    let entrada = document.getElementById('entrada');
+    let mostrar = document.getElementById('mostrar');
+    let valor = entrada.value;
+    mostrar.innerText = valor;
+    entrada.value = '';
+
+    //após obtermos o dado do usuário
+    //vamos guardá-lo no local storage
+    sessionStorage.setItem('dadoDoUsuario', valor);
+}
+
+//Para recuperar o dado de local storage
+//usamo o sessionStorage.getItem()
+document.querySelector('#mostrar').innerText =
+    sessionStorage.getItem('dadoDoUsuario');
+
+//E para apagar no session storage?
+//removeItem()
+//sessionStorage.removeItem('dadoDoUsuario'); 
