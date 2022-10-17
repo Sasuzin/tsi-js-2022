bbce5e0a8a40cc95783b0325b29230b263f34187
Luís Felipe Galvão <88863600+Sasuzin@users.noreply.github.com>
2022-10-17 17:07:48 -0300

    Update ex8DOM.js

--- JS/ex8DOM.js
+++ JS/ex8DOM.js
@@ -1,11 +1,18 @@
-const card = document.querySelector('.card');
+const cartao = document.querySelector('.card');
 const titulo = document.querySelector('h5');
 
-card.addEventListener('mousemove', mostrarEvento);
+cartao.addEventListener('mousemove', executaAcao);
 
-function mostrarEvento(evento) {
+function executaAcao(evento){
 
-    console.log(evento);
+    //console.log(evento);
 
-    titulo.textContent = `Eixo X: ${evento.offsetX} Eixo Y: ${evento.offsetY}`;
+    titulo.textContent = 
+        `Eixo X: ${evento.offsetX} 
+         Eixo Y: ${evento.offsetY}`;
+
+    document.body.style.backgroundColor = 
+        `rgb(${evento.offsetX}, 
+            ${evento.offsetY}, 
+            40)`;
 }
