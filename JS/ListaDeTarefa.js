2505cb0224a1970e83d64a01c6b21518a91b1151
Your Name <you@example.com>
2022-10-17 17:18:13 -0300

    App Lista de tarefas funcionando o inserir e o apagar

---
+++ JS/ListaDeTarefa.js
@@ -0,0 +1,57 @@
+const entradaTarefa = document.querySelector('#tarefa');
+const btnAddTarefa = document.querySelector('.btn');
+const listaDeTarefas = document.querySelector('.collection');
+const filtroDeTarefa = document.querySelector('#filtro');
+const btnLimpaTudo = document.querySelector('.limpar-tarefas');
+
+function carregaMonitoresDeEventos(){
+
+    //evento para adicionar a tarefa
+    btnAddTarefa.addEventListener('click', adicionarTarefa);
+    //evento para fazer o x funcionar (apagar uma única tarefa)
+    listaDeTarefas.addEventListener('click', apagarTarefa);
+}
+carregaMonitoresDeEventos();
+
+function apagarTarefa(evento){
+
+    //se o elemento pai tiver a classe apaga-tarefa,
+    //ou seja, for o elemento "a", apague li, ou seja, 
+    //a tarefa
+    if( evento.target.parentElement.classList.contains('apaga-tarefa') ){
+
+        //apaga o elemento pai do "a", ou seja, o li, pois
+        //o elemento alvo do evento foi o i 
+        evento.target.parentElement.parentElement.remove();
+    }
+}
+
+function adicionarTarefa(evento){
+    evento.preventDefault();
+
+    //verifica se usuário entrou com uma tarefa
+    if( entradaTarefa.value === '' ){
+        alert('Entre com uma tarefa');
+    }
+
+    //cria li com a nova tarefa
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    li.appendChild(document.createTextNode(entradaTarefa.value));
+
+    //cria a onde vai ficar o x para apagar a tarefa
+    const a = document.createElement('a');
+    a.className = 'apaga-tarefa secondary-content';
+
+    //cria ícone com x para apagar a tarefa
+    const i = document.createElement('i');
+    i.className = 'fa fa-remove';
+
+    //monta o elemento li para colocar em ul
+    a.appendChild(i);
+    li.appendChild(a);
+    listaDeTarefas.appendChild(li);
+
+    //apaga o input para entrada
+    entradaTarefa.value = '';
+}
--- JS/sessionStorage.js
+++ JS/sessionStorage.js
@@ -25,4 +25,37 @@ document.querySelector('#mostrar').innerText =
 
 //E para apagar no session storage?
 //removeItem()
-//sessionStorage.removeItem('dadoDoUsuario'); 
+//sessionStorage.removeItem('dadoDoUsuario');   
+
+let vetor = ['pera','banana','manga'];
+
+//Para gardar algo que não seja uma string
+//no localStorage ou sessionStorage, temos
+//que converter para string. Com o método
+//JSON.stringify() transformamos vetores
+//e objetos em string
+sessionStorage.setItem( 'frutas', 
+                        JSON.stringify(vetor));
+
+//Para recuperar uma string JSON do localStorage
+//ou sessionStorage e transformar em um objeto
+//ou vetor novamente, use JSON.parse()
+let recupDado = JSON.parse(
+                    sessionStorage.
+                        getItem('frutas'));
+
+//Alterações no vetor recuperado 
+//do sessionStorage
+recupDado[3] = 'morango';
+recupDado[1] = 'parchita';
+
+//console.log(recupDado);
+
+//Transforma o vetor em string para poder
+//gravar em sessionStorage ou localStorage
+let strRecupDados = JSON.stringify(recupDado);
+
+//console.log(strRecupDados);
+
+//Atualiza o item frutas no sessionStorage
+sessionStorage.setItem('frutas', strRecupDados );
