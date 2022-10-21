6ef5e99f8ffb2b7d6a48a8ddc316aa050e95ddc6
LUIS FELIPE GALVAO TORRADO <luis.fgtorrado@senacsp.edu.br>
2022-10-21 20:40:45 -0300

    Primeira parte da aula

--- JS/ListaDeTarefa.js
+++ JS/ListaDeTarefa.js
@@ -1,57 +1,71 @@
-const entradaTarefa = document.querySelector('#tarefa');
-const btnAddTarefa = document.querySelector('.btn');
-const listaDeTarefas = document.querySelector('.collection');
-const filtroDeTarefa = document.querySelector('#filtro');
-const btnLimpaTudo = document.querySelector('.limpar-tarefas');
+const entradaTarefa = document.querySelector("#tarefa");
+const btnAddTarefa = document.querySelector(".btn");
+const listaDeTarefas = document.querySelector(".collection");
+const filtroDeTarefa = document.querySelector("#filtro");
+const btnLimpaTudo = document.querySelector(".limpar-tarefas");
 
-function carregaMonitoresDeEventos(){
+function carregaMonitoresDeEventos() {
+    // Evento para adicionar a tarefa
+    btnAddTarefa.addEventListener("click", adicionarTarefa);
 
-    //evento para adicionar a tarefa
-    btnAddTarefa.addEventListener('click', adicionarTarefa);
-    //evento para fazer o x funcionar (apagar uma única tarefa)
-    listaDeTarefas.addEventListener('click', apagarTarefa);
+    //Filtrar tarefa
+    filtroDeTarefa.addEventListener("keyup", filtrarTarefa);
+
+    // Evento para apagar uma única tarefa
+    listaDeTarefas.addEventListener("click", apagarTarefa);
+
+    listaDeTarefas.addEventListener("click", apagarTudo);
 }
 carregaMonitoresDeEventos();
 
-function apagarTarefa(evento){
+function apagarTudo(e) {
+    e.preventDefault();
 
-    //se o elemento pai tiver a classe apaga-tarefa,
-    //ou seja, for o elemento "a", apague li, ou seja, 
-    //a tarefa
-    if( evento.target.parentElement.classList.contains('apaga-tarefa') ){
+    listaDeTarefas.innerHTML = '';
+}
 
-        //apaga o elemento pai do "a", ou seja, o li, pois
-        //o elemento alvo do evento foi o i 
-        evento.target.parentElement.parentElement.remove();
+function apagarTarefa(e) {
+    // Se o elemento pai for o elemento <a>, apaga o elemento <li>, ou seja, a tarefa    
+    if (e.target.parentElement.classList.contains("apaga-tarefa")) {
+        e.target.parentElement.parentElement.remove();
     }
 }
 
-function adicionarTarefa(evento){
-    evento.preventDefault();
+function adicionarTarefa(e) {
+    e.preventDefault();
 
-    //verifica se usuário entrou com uma tarefa
-    if( entradaTarefa.value === '' ){
-        alert('Entre com uma tarefa');
+    // Verifica se usuário digitou algo
+    if (entradaTarefa.value === "" || !entradaTarefa) {
+        alert("Digite uma tarefa!");
     }
 
-    //cria li com a nova tarefa
-    const li = document.createElement('li');
-    li.className = 'collection-item';
+    // Cria <li> com a nova tarefa
+    const li = document.createElement("li");
+    li.className = "collection-item";
     li.appendChild(document.createTextNode(entradaTarefa.value));
 
-    //cria a onde vai ficar o x para apagar a tarefa
-    const a = document.createElement('a');
-    a.className = 'apaga-tarefa secondary-content';
+    // Cria a onde vai ficar o botão de apagar para a tarefa
+    const a = document.createElement("a");
+    a.className = "apaga-tarefa secondary-content";
 
-    //cria ícone com x para apagar a tarefa
-    const i = document.createElement('i');
-    i.className = 'fa fa-remove';
+    // Cria ícone com "X" para apagar a tarefa
+    const i = document.createElement("i");
+    i.className = "fa fa-remove";
 
-    //monta o elemento li para colocar em ul
+    // Monta o elemento <li>
     a.appendChild(i);
     li.appendChild(a);
+
+    // Adicionando o <li> na <ul>
     listaDeTarefas.appendChild(li);
 
-    //apaga o input para entrada
-    entradaTarefa.value = '';
+    // Apaga os dados digitados no input
+    entradaTarefa.value = "";
 }
+
+function filtrarTarefa(e) {
+        const procurado = e.target.value.toLowerCase();
+
+        
+}
+carregaMonitoresDeEventos();
