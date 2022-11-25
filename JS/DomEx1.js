b0f49d1f06233a4fdb7bf659465d5204a59dc07c
LUIS FELIPE GALVAO TORRADO <luis.fgtorrado@senacsp.edu.br>
2022-11-25 20:42:17 -0200

    Ultima aula, finalização da API do Grafico

---
+++ JS/DomEx1.js
@@ -0,0 +1,12 @@
+console.log(window);    
+
+//Como utilizar atributos de um objeto
+//alert(`Sua resoluçã é: ${window.innerHeight} x ${window.innerWidth}`);
+
+let meuTitulo = document.getElementById('titulo');
+
+meuTitulo.innerText = 'Mudou';
+
+meuTitulo.style.background ='#836FFF';
+
+console.log('meuTitulo');
--- grafico/ipca.js
+++ grafico/ipca.js
@@ -3,6 +3,8 @@ document.querySelector('button').addEventListener('click', mostraIpca);
 function mostraIpca(evento){
     evento.preventDefault();
 
+    document.querySelector('#ipca').innerHTML = '';
+
     const url = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.4448/dados?formato=json&dataInicial=20201101';
 
     fetch(url).then(function(retorno){
@@ -11,33 +13,44 @@ function mostraIpca(evento){
 
     }).then(function(stringJson){
 
+        let anoUsuario = document.querySelector('#ano').value;
+
         const indices = JSON.parse(stringJson);
 
-        //////  Fazer em casa! //////
-        //colocar os dados recuperados da API 
-        //no gráfico
+        let indice;
+        let ipca = [];
+
+        indices.forEach( function( mes ){
+
+            anoString = mes.data.substring(6, 10);
+            mesString = mes.data.substring(3, 5);
+            anoMesString = anoString + '-' + mesString;
+
+            if( parseInt(anoString) != anoUsuario && 
+                anoUsuario != ""){
+                return;
+            }
+
+            indice = parseFloat(mes.valor);
+
+            ipca.push({ month: anoMesString, value: indice});
+        });
+
+        if(ipca.length == 0){
+
+            alert(`Não há dados para o ano ${anoUsuario}`);
+        }
 
         jsonParaMorris = {
                             element: 'ipca',
-                                // Dados do gráfico
-                            data: [
-                                { year: '2008', value: 20 },
-                                { year: '2009', value: 10 },
-                                { year: '2010', value: 5 },
-                                { year: '2011', value: 5 },
-                                { year: '2012', value: 20 },
-                                { year: '2013', value: 25 },
-                                { year: '2014', value: 7 }
-                            ],
-                        
+                            // Dados do gráfico
+                            data: ipca,
                             // O nome do eixo com os valores de X
-                            xkey: 'year',
-                            
+                            xkey: 'month',
                             // Uma lista de nomes dos atributos de dados contidos em Y
                             ykeys: ['value'],
-                        
                             // Rótulos para os índices -- aparece quando passa o mouse por cima
-                            labels: ['Value']
+                            labels: ['IPCA']
                         };
 
         new Morris.Line(jsonParaMorris);
